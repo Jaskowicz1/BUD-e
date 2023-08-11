17ac6e881a6986780d35a033f6caeb3cc936d18c
Jaskowicz1 <46899449+Jaskowicz1@users.noreply.github.com>
2023-08-11 13:23:47 +0100

    Update UserInfoCommand.h

--- src/Commands/UserInfoCommand.h
+++ src/Commands/UserInfoCommand.h
@@ -14,11 +14,6 @@ public:
         dpp::embed embed(EmbedBuilder::BasicEmbed(dpp::colours::aqua,"Showing information for: '" + event.command.get_issuing_user().username + "':",
                                  "This information is based on your account. If anything seems incorrect, contact the team (you can find me with /credits)!"));
 
-        dpp::embed_field nitroField;
-        nitroField.name = "Has Nitro:";
-        nitroField.value = event.command.get_issuing_user().has_nitro_basic() || event.command.get_issuing_user().has_nitro_classic() || event.command.get_issuing_user().has_nitro_full();
-        nitroField.is_inline = true;
-
         dpp::embed_field dateField;
         dateField.name = "Date created:";
         dateField.value = event.command.get_issuing_user().get_creation_time();
@@ -26,10 +21,10 @@ public:
 
         dpp::embed_field idField;
         idField.name = "User ID:";
-        idField.value = event.command.get_issuing_user().id;
+        idField.value = event.command.get_issuing_user().id.str();
         idField.is_inline = true;
 
-        EmbedBuilder::AddFieldToEmbed(embed, nitroField);
+        //EmbedBuilder::AddFieldToEmbed(embed, nitroField);
         EmbedBuilder::AddFieldToEmbed(embed, dateField);
         EmbedBuilder::AddFieldToEmbed(embed, idField);
 
