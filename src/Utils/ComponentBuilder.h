f431b03b07b529f88373a2ca358398425fe57b73
Jaskowicz1 <46899449+Jaskowicz1@users.noreply.github.com>
2023-08-11 11:02:15 +0100

    Fixed an error with ComponentBuilder.

--- src/Utils/ComponentBuilder.h
+++ src/Utils/ComponentBuilder.h
@@ -10,17 +10,15 @@ public:
 
         dpp::component component;
 
-        component.add_component(dpp::component().
-            set_label(label).
-            set_type(dpp::cot_button).
-            set_emoji(emoji).
-            set_style(dpp::cos_primary).
-            set_id(id)
-        );
+        component.set_label(label).
+        set_type(dpp::cot_button).
+        set_emoji(emoji).
+        set_style(dpp::cos_primary).
+        set_id(id);
 
         // If we have no components, add the component to the message.
         if(message.components.empty())
-            message.add_component(component);
+            message.add_component(dpp::component().add_component(component));
         else // otherwise, add the comp to the component. Otherwise, we end up with the buttons being on new lines.
             message.components[0].add_component(component);
 
