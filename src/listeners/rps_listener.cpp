d090f7e79efa9969607828f6517b33a5667fbc31
Archie Jaskowicz <archiejaskowicz@gmail.com>
2025-03-20 20:32:22 +0000

    fix: Rock paper scissors now works correctly

--- src/listeners/rps_listener.cpp
+++ src/listeners/rps_listener.cpp
@@ -28,16 +28,15 @@ dpp::task<void> rps_listener::on_button_click(dpp::button_click_t event) {
 
     // If the event wasn't rock, nor paper, nor scissors, then just return.
     // This handler is not made for anything else, and we've already handled playing again (if that was the case).
-    if(event.custom_id != "rock" && event.custom_id != "paper" && event.custom_id != "scissors")
+    if (event.custom_id != "rock" && event.custom_id != "paper" && event.custom_id != "scissors") {
         co_return;
+    }
 
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> distrib(0, 99);
+    std::uniform_int_distribution<int> distrib(0, 2);
 
-    // This will return either 0, 1 or 2. Going from 0-99 means we have more to play with as
-    // 0-2 was yielding results that did not feel random.
-    int aiChoiceNUM = (distrib(gen)/33) - 1;
+    int aiChoiceNUM = distrib(gen);
 
     std::string aiChoice;
     std::string aiChoiceEmoji;
@@ -71,33 +70,40 @@ dpp::task<void> rps_listener::on_button_click(dpp::button_click_t event) {
         playerChoiceEmoji = dpp::unicode_emoji::scissors;
     }
 
-    // if BUD-e picked rock and player picked paper.
-    if(aiChoice == "rock" && event.custom_id == "paper") {
-        result = "You won!";
-        resultColour = dpp::colours::green;
-    } else if(aiChoice == "rock" && event.custom_id == "scissors") {
-        result = "I won!";
-        resultColour = dpp::colours::red;
-    } else if (aiChoice == "paper" && event.custom_id == "scissors") {
-        result = "You Won!";
-        resultColour = dpp::colours::green;
-    } else if (aiChoice == "paper" && event.custom_id == "rock") {
-        result = "I Won!";
-        resultColour = dpp::colours::red;
-    } else if (aiChoice == "scissors" && event.custom_id == "rock") {
-        result = "You won!";
-        resultColour = dpp::colours::green;
-    } else if (aiChoice == "scissors" && event.custom_id == "paper") {
-        result = "I won!";
-        resultColour = dpp::colours::red;
+    if (aiChoice == "rock") { // BUD-e chose Rock
+        if (event.custom_id == "paper") {
+            result = "You won!";
+            resultColour = dpp::colours::green;
+        } else if (event.custom_id == "scissors") {
+            result = "I won!";
+            resultColour = dpp::colours::red;
+        }
+    } else if (aiChoice == "paper") { // BUD-e chose Paper
+        if (event.custom_id == "scissors") {
+            result = "You Won!";
+            resultColour = dpp::colours::green;
+        } else if (event.custom_id == "rock") {
+            result = "I Won!";
+            resultColour = dpp::colours::red;
+        }
+    } else if (aiChoice == "scissors") { // BUD-e chose Scissors
+        if (event.custom_id == "rock") {
+            result = "You won!";
+            resultColour = dpp::colours::green;
+        } else if (event.custom_id == "paper") {
+            result = "I won!";
+            resultColour = dpp::colours::red;
+        }
     } else { // Assume that player picked the same as BUD-e.
         result = "Draw!";
         resultColour = dpp::colours::yellow;
     }
 
     dpp::message msg(event.command.channel_id,
-		     embed_builder::basic_embed(resultColour, result,
-                                              "I picked " + aiChoiceEmoji + " and you picked " + playerChoiceEmoji + "! Press the button below to play again!")
+        embed_builder::basic_embed(
+        resultColour, result,
+        "I picked " + aiChoiceEmoji + " and you picked " + playerChoiceEmoji + "! Press the button below to play again!"
+        )
     );
 
     component_builder::add_button_to_message(msg, component_data{"Play Again", "playagain", dpp::unicode_emoji::video_game});
